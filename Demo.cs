4907826303cdb5b8bb6d8bd97f2d12aae53350f1
Jahna <janu310703@gmail.com>
2024-07-10 17:33:52 +0530

    Demo.cs Committed

---
+++ Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Demo1
+{
+    internal class Demo
+    {
+        public static int staticCount;
+        public int intCount;
+
+        public void LocalVariableDemo()
+        {
+            intCounts = 100;
+            Console.WriteLine("Count: {0}", count);
+        }
+    }
+}
